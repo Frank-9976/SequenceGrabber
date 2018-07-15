b46543116d54a7908f89c2f31d6e52fba4bb6976
Frank-9976 <41272099+Frank-9976@users.noreply.github.com>
2018-07-15 16:37:19 -0700

    Not Tested - Type Option

--- SequenceGrabber.py
+++ SequenceGrabber.py
@@ -9,8 +9,14 @@ def IsInteger(x):
     except ValueError: return False
     else: return True
     
-def DownloadSequence(filePath, ID):
-    URL = f'https://onlinesequencer.net/app/midi.php?id={ID}'
+def DownloadSequence(filePath, ID, Type):
+    if Type == 'mid':
+        URL = f'https://onlinesequencer.net/app/midi.php?id={ID}'
+    elif Type == 'json':
+        URL = f'https://onlinesequencer.net/ajax/load.php?id={ID}'
+    else:
+        print('ERROR: UNKNOWN TYPE')
+        return
     data = urlopen(URL).read()
     with open(filePath, 'wb') as f:
         f.write(data)
@@ -25,10 +31,13 @@ def main():
     else:
         print('USAGE: SequenceGrabber <User ID>')
         return
-    #Get minDate and maxDate
+    #Get minDate, maxDate, and type
     #May add command line support in the future
     minDate = input('Minimum sequence id to retrieve? (0 for no minimum) ')
     maxDate = input('Maximum sequence id to retrieve? (-1 for no maximum) ')
+    Type = input('Type mid for midis and json for pure ajax data. ')
+    if Type not in ('mid', 'json'):
+        print('Invalid type.')
     
     #Ensure that the user ID, minDate, and maxDate is a number
     if not IsInteger(userID):
@@ -54,7 +63,7 @@ def main():
         filePath = f'{userID}\\{fileName}'
         if not os.path.isfile(filePath):
             print(f'Downloading {fileName}')
-            DownloadSequence(filePath, seqID)
+            DownloadSequence(filePath, seqID, Type)
         
 
         
